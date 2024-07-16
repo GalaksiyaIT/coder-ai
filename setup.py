120b54e258916a211b7a238d579b0ab0d90f5ffc
dilsenyh <dilsenyildar@gmail.com>
2024-07-16 21:18:45 +0300

    updated.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent"
     ],
     python_requires='>=3.9',
